376aed4575bbbab9530b8456dcb3275f05aa7f54
salahmohamed0944 <salahmohamed0944@gmail.com>
2022-02-01 21:34:09 +0100

    first-commit

---
+++ src/components/Forms.js
@@ -0,0 +1,66 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import Card from '../components/Card'
+
+const Forms = () => {
+    const [moviesData, setMoviesData] = useState([]);
+    const [search, setSearch] = useState("code");
+    const [sortGoodToBad, setSortGoodToBad] = useState("goodToBad")
+    useEffect(() => {
+        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=ed82f4c18f2964e75117c2dc65e2161d&query=${search}&language=fr-FR`
+        )
+            .then((res) =>
+
+                setMoviesData(res.data.results))
+
+    }, [search])
+    return (
+        <div className='form-component'>
+            <div className='form-container'>
+                <form>
+                    <input type="text"
+
+                        placeholder="Entrez le titre d'un film"
+                        id="search-input"
+                        onChange={(e) => setSearch(e.target.value)} />
+                    <input type="submit" value="Rechcercher" />
+
+                </form>
+                <div className="btn-sort-container">
+                    <div
+                        className="btn-sort"
+                        id="goodToBad" onClick={()=>setSortGoodToBad('goodToBad')}
+
+                    >
+                        Top<span>➜</span>
+                    </div>
+                    <div
+                        className="btn-sort"
+                        id="badToGood" onClick={()=>setSortGoodToBad('badToGood')}
+
+                    >
+                        Flop<span>➜</span>
+                    </div>
+                </div>
+            </div>
+            <div className='result'>
+
+                {moviesData.slice(0, 12)
+                    .sort(
+                        (a, b) => {
+                            if (sortGoodToBad === "goodToBad") {
+                                return b.vote_average - a.vote_average
+                            }else if(sortGoodToBad === "badToGood") {
+                                return a.vote_average - b.vote_average
+                            }
+                        }
+                    )
+                    .map((movie) =>
+                        <Card key={movie.id} movie={movie} />
+                    )}
+            </div>
+        </div>
+    );
+};
+
+export default Forms;
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../components/Header'
+import Forms from '../components/Forms'
+
+function Home() {
+    return (
+        <div>
+            <Header/>
+             <Forms />
+            
+        </div>
+    )
+}
+
+export default Home
